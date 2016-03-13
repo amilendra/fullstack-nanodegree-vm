abb4175c37821db23b0c6ac9c6bcc3c4517c4ca0
Amilendra Kodithuwakku <amilendra.kodithuwakku@gmail.com>
2016-03-13 22:54:18 +0900

    Committing my Submission for project2

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,22 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+-- Clean up
+DROP TABLE IF EXISTS matches;
+DROP TABLE IF EXISTS players;
+--DROP DATABASE IF EXISTS tournament;
 
+-- Create the tournament database
+--CREATE DATABASE tournament;
+
+-- Connect to the tournament database
+\connect tournament;
+
+-- Create tables
+CREATE TABLE IF NOT EXISTS players (id serial NOT NULL PRIMARY KEY, name text NOT NULL);
+CREATE TABLE IF NOT EXISTS matches (id serial NOT NULL PRIMARY KEY, loser_id integer NOT NULL, winner_id integer NOT NULL);
+
+ALTER TABLE matches ADD CONSTRAINT matches_loser_id_fk FOREIGN KEY (loser_id) REFERENCES players (id) DEFERRABLE INITIALLY DEFERRED;
+ALTER TABLE matches ADD CONSTRAINT matches_winner_id_fk FOREIGN KEY (winner_id) REFERENCES players (id) DEFERRABLE INITIALLY DEFERRED;
+
+SELECT * FROM players WHERE id NOT IN ( SELECT winner_id FROM matches GROUP BY winner_id );
